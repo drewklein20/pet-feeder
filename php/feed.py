6f4c10fc3e230f0e4172fdcee856220e2ceda341
Drew Klein <drew.klein@doane.edu>
2021-03-16 12:47:06 -0500

    updated python script for feeding

--- php/feed.py
+++ php/feed.py
@@ -12,31 +12,38 @@ mydb = mysql.connector.connect(
   database="Feeder"
 )
 
+isUsingScale = False
+twoBowls = False
+currentWeight = 0
+fullBowlWeight = 0
+
 dbcursor = mydb.cursor()
-dbcursor.execute("SELECT * FROM Feeder.Weights order by id desc limit 1;")
+dbcursor.execute("SELECT JSON_UNQUOTE(JSON_EXTRACT(preferences, '$.cupDuration')) as cupDuration, JSON_UNQUOTE(JSON_EXTRACT(preferences, '$.bowlWeight')) as cupDuration, JSON_UNQUOTE(JSON_EXTRACT(preferences, '$.twoBowls')) as twoBowls, JSON_UNQUOTE(JSON_EXTRACT(preferences, '$.isUsingScale')) as isUsingScale FROM Feeder.Settings;")
 dbresult = dbcursor.fetchone()
-currentWeight = dbresult[1]
+cupDuration = dbresult[0]
+
+if dbresult[2] == 'true':
+	twoBowls = True
+if dbresult[3] == 'true':
+	isUsingScale = True
+	fullBowlWeight = dbresult[1]
+
+	dbcursor = mydb.cursor()
+	dbcursor.execute("SELECT * FROM Feeder.Weights order by id desc limit 1;")
+	dbresult = dbcursor.fetchone()
+	currentWeight = dbresult[1]
 
 dbcursor = mydb.cursor()
 dbcursor.execute("SELECT * FROM Logs ORDER BY id desc LIMIT 1")
 dbresult = dbcursor.fetchone()
 feedCups = dbresult[2]
 
-dbcursor = mydb.cursor()
-dbcursor.execute("SELECT JSON_UNQUOTE(JSON_EXTRACT(preferences, '$.cupDuration')) as cupDuration, JSON_UNQUOTE(JSON_EXTRACT(preferences, '$.bowlWeight')) as cupDuration, JSON_UNQUOTE(JSON_EXTRACT(preferences, '$.twoBowls')) as twoBowls FROM Feeder.Settings;")
-dbresult = dbcursor.fetchone()
-cupDuration = dbresult[0]
-fullBowlWeight = dbresult[1]
-print(dbresult[2])
-twoBowls = False
-
-if dbresult[2] == 'true':
-	twoBowls = True
-
-sleepAmount = float(cupDuration) * float(feedCups)
 print(twoBowls)
 print(currentWeight)
 print(fullBowlWeight)
+
+sleepAmount = float(cupDuration) * float(feedCups)
+
 if currentWeight < fullBowlWeight:
 	try:
 		pi.set_servo_pulsewidth(17, 2000)
